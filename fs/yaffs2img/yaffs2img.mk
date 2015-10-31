4ddfc11d5493a31a6d4cf681d52cc30eee7f4100
Han Pengfei <pengphei@sina.com>
2015-11-01 01:19:00 +0800

    add yaffs2img support

---
+++ fs/yaffs2img/yaffs2img.mk
@@ -0,0 +1,13 @@
+################################################################################
+#
+# Build the yaffs2img root filesystem image
+#
+################################################################################
+
+ROOTFS_YAFFS2IMG_DEPENDENCIES = host-yaffs2
+
+define ROOTFS_YAFFS2IMG_CMD
+	$(HOST_DIR)/usr/bin/mkyaffs2img --all-root $(TARGET_DIR) $@
+endef
+
+$(eval $(call ROOTFS_TARGET,yaffs2img))
---
+++ package/yaffs2/yaffs2.mk
@@ -0,0 +1,21 @@
+################################################################################
+#
+# yaffs2
+#
+################################################################################
+
+YAFFS2_VERSION = unknown
+YAFFS2_SOURCE = yaffs2-source.tar.gz
+YAFFS2_SITE = ftp://sourceforge.nchc.org.tw/f/fa/fatplus/
+YAFFS2_LICENSE = GPLv2
+YAFFS2_LICENSE_FILES = COPYING
+
+define HOST_YAFFS2_BUILD_CMDS
+	$(HOST_MAKE_ENV) $(MAKE) -C $(@D)/utils
+endef
+
+define HOST_YAFFS2_INSTALL_CMDS
+	$(HOST_MAKE_ENV) $(MAKE) -C $(@D)/utils INSTALLDIR=$(HOST_DIR)/usr/bin install
+endef
+
+$(eval $(host-generic-package))
